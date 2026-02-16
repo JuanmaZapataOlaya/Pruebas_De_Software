4214a2d3b81bda2d918feba7501eb4c5edb4c3b3
JuanmaZapataOlaya <124525725+JuanmaZapataOlaya@users.noreply.github.com>
2026-02-16 22:56:32 +0000

    Arreglo

--- test_es_multiplo.py
+++ test_es_multiplo.py
@@ -2,18 +2,15 @@ import unittest
 from math_utils import es_multiplo
 
 class Testesmultiplo(unittest.TestCase):
-    def es_multiplo_6_2(self):
+    def test_es_multiplo_6_2(self):
         self.assertTrue(es_multiplo(6,2))
 
-    def es_multiplo_menos6_2(self):
+    def test_es_multiplo_menos6_2(self):
         self.assertTrue(es_multiplo(-6,2))
 
-    def es_multiplo_0_3(self):
+    def test_es_multiplo_0_3(self):
         self.assertTrue(es_multiplo(0,3))
 
-    def es_multiplo_5_3(self):
-        self.assertTrue(es_multiplo(5,3))
+    def test_es_multiplo_5_3(self):
+        self.assertFalse(es_multiplo(5,3))
 
-
-if __name__ == "__main__":
-    unittest.main()
